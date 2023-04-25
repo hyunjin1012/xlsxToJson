8aff5eba37621dd5ed5cad95d6d4a0a4ceab49e0
hyunjin1012 <hyunjin1012@gmail.com>
2023-04-24 20:32:05 -0700

    build csv-json converter

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,185 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import JSZip from "jszip";
+import { Inter } from "next/font/google";
+import { useEffect, useState } from "react";
+import { saveAs } from "file-saver";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [file, setFile] = useState(null);
+  const [json, setJson] = useState(null);
+  const [name, setName] = useState("");
+  const [RMNTUrl, setRMNTUrl] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [artist, setArtist] = useState("");
+  const [license, setLicense] = useState("");
+
+  let fileReader;
+  useEffect(() => {
+    fileReader = new FileReader();
+  });
+
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleRMNTUrlChange = (e) => {
+    setRMNTUrl(e.target.value);
+  };
+
+  const handleImageUrlChange = (e) => {
+    setImageUrl(e.target.value);
+  };
+
+  const handleArtistChange = (e) => {
+    setArtist(e.target.value);
+  };
+
+  const handleLicenseChange = (e) => {
+    setLicense(e.target.value);
+  };
+
+  const csvFileToArray = (string) => {
+    var array = string.toString().split("\r\n");
+    var data = [];
+    for (const r of array) {
+      let row = r.toString().split(",");
+      data.push(row);
+    }
+    var heading = data[0];
+    var result = [];
+    for (var i = 1; i < data.length; i++) {
+      var row = data[i];
+      var obj = { trait_type: "", value: "" };
+      var attributes = [];
+      for (var j = 1; j < heading.length; j++) {
+        if (row[j] != "") {
+          obj.trait_type = heading[j].replaceAll(" ", "_");
+          obj.value = row[j].toString().replaceAll(" ", "_");
+
+          attributes.push(JSON.parse(JSON.stringify(obj)));
+        }
+      }
+      result.push({
+        name: name + " #" + row[0],
+        external_url: RMNTUrl,
+        image: imageUrl,
+        artist: artist,
+        license: license,
+        attributes: attributes,
+      });
+    }
+    console.log(result);
+    setJson(result);
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if (file) {
+      fileReader.onload = function (event) {
+        const text = event.target.result;
+        csvFileToArray(text);
+      };
+      fileReader.readAsText(file);
+    }
+  };
+
+  const exportFile = () => {
+    const element = document.createElement("a");
+    const file = new Blob([JSON.stringify(json, null, 2)], {
+      type: "text/plain",
+    });
+    element.href = URL.createObjectURL(file);
+    element.download = "json.json";
+    document.body.appendChild(element); // Required for this to work in FireFox
+    element.click();
+  };
+
+  const exportZip = () => {
+    const zip = new JSZip();
+    for (let i = 0; i < json.length; i++) {
+      const file = new Blob([JSON.stringify(json[i], null, 2)], {
+        type: "text/plain",
+      });
+      zip.file(json[i].name.split("#")[1] + ".json", file);
+    }
+    zip.generateAsync({ type: "blob" }).then(function (content) {
+      saveAs(content, "json.zip");
+    });
+  };
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+      <h1 className="text-4xl font-bold text-center">CSV to JSON</h1>
+      <div className="flex flex-col items-center justify-center mt-4">
+        {json && (
+          <div className="py-8 flex flex-col items-center justify-center gap-4">
+            <div className="flex gap-1">
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                type="button"
+                onClick={exportFile}
+              >
+                Download in a single file
+              </button>
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                type="button"
+                onClick={exportZip}
+              >
+                Download in multiple files
+              </button>
+            </div>
+            <pre className="flex flex-col items-center justify-center text-sm">
+              {json && JSON.stringify(json, null, 2)}
+            </pre>
+          </div>
+        )}
+
+        <form className="flex flex-col gap-2">
+          <input
+            type="text"
+            placeholder="Enter the character name"
+            onChange={handleNameChange}
+          />
+          <input
+            type="text"
+            placeholder="Enter the RMNT url"
+            onChange={handleRMNTUrlChange}
+          />
+          <input
+            type="text"
+            placeholder="Enter the image url"
+            onChange={handleImageUrlChange}
+          />
+          <input
+            type="text"
+            placeholder="Enter the artist name"
+            onChange={handleArtistChange}
+          />
+          <input
+            type="text"
+            placeholder="Enter the license description"
+            onChange={handleLicenseChange}
+          />
+          <div className="flex items-center justify-center gap-1">
+            <label>CSV File: </label>
+            <input type="file" onChange={handleFileChange} />
+          </div>
+          <button
+            onClick={handleOnSubmit}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+            Convert
+          </button>
+        </form>
       </div>
     </main>
-  )
+  );
 }
